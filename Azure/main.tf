62d70f6c1bc2e94ff7071ee4690d40fef293bbd4
michalantolik <michal.antolik@gmail.com>
2022-10-13 13:33:15 +0200

    Create AZ virtual network

--- Azure/main.tf
+++ Azure/main.tf
@@ -14,8 +14,16 @@ provider "azurerm" {
   features {}
 }
 
-
+# Create resource group
 resource "azurerm_resource_group" "main" {
     name = "learn-tf-rg-westeurope"
     location = "westeurope"
+}
+
+# Create VNet
+resource "azurerm_virtual_network" "main" {
+    name = "learn-tf-vnet-westus"
+    location = azurerm_resource_group.main.location
+    resource_group_name = azurerm_resource_group.main.name
+    address_space = [ "10.0.0.0/16" ]
 }
